e49da21d33e57fa033db970bd8a818b97cc4c222
chrn1y <artyom.sushkov@gmail.com>
2020-11-15 00:32:04 +0300

    minor changes

--- backend/main.py
+++ backend/main.py
@@ -11,14 +11,17 @@ app = Flask(__name__)
 # returns jason with mrz and mrz transcription
 @app.route("/", methods=['POST'])
 def image():
-    image_str = request.get_json()['image']
-    image_orig = base64.b64decode(image_str)
-    image_np = numpy.frombuffer(image_orig, dtype=numpy.uint8)
-    image_cv2 = cv2.imdecode(image_np, flags=1)
-    response = get_json_mrz(image_cv2)
-    if response is None:
+    if request.headers.get('content-type') == 'application/json':
+        image_str = request.get_json()['image']
+        image_orig = base64.b64decode(image_str)
+        image_np = numpy.frombuffer(image_orig, dtype=numpy.uint8)
+        image_cv2 = cv2.imdecode(image_np, flags=1)
+        response = get_json_mrz(image_cv2)
+        if response is None:
+            abort(418)
+        return Response(response, mimetype='application/json')
+    else:
         abort(418)
-    return Response(response, mimetype='application/json')
 
 
 if __name__ == '__main__':
