62c489ec41ee181c1e173876f541c142a952f240
chrn1y <artyom.sushkov@gmail.com>
2020-11-14 03:33:57 +0300

    added test func

--- back/combiner.py
+++ back/combiner.py
@@ -1,3 +1,4 @@
+import cv2
 import base64
 import io
 from imageio import imread
@@ -10,3 +11,6 @@ def get_mrz_from_b64_bytes(data):
     b64_string = b64_bytes.decode()
     img = imread(io.BytesIO(base64.b64decode(b64_string)))
     return get_mrz_data(get_mrq_area(img))
+
+def test_mrz():
+    return get_mrz_data(get_mrq_area(cv2.imread("./photo_2020-11-13_22-17-31.jpg")))
--- back/main.py
+++ back/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from combiner import get_mrz_from_b64_bytes
+from combiner import get_mrz_from_b64_bytes, test_mrz
 
 app = Flask(__name__)
 
